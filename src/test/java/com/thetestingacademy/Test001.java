dd76120ebc8780eae0e6c52ce76b46900b651b00
Shubham Khera <shubham_khera@hotmail.com>
2024-11-09 17:38:23 +0530

    1

---
+++ src/test/java/com/thetestingacademy/Test001.java
@@ -0,0 +1,8 @@
+package com.thetestingacademy;
+
+public class Test001 {
+    public static void main(String[] args) {
+        // Rest Assured
+        System.out.println("Testing Rest Assured");
+    }
+}
